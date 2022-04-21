2ae5f1057becd9d845353f07249a406877373da7
tuxerrante <affinito.ale@gmail.com>
2022-04-21 12:47:42 +0200

    functions

---
+++ type-test/slices/slices.go
@@ -0,0 +1,35 @@
+package main
+import "fmt"
+
+func main (){
+	s := [3]int{10, 20, 30}
+	fmt.Println(s)
+
+	modify(s[:])
+	fmt.Println("Will change the reference, since it is passed as slice", s)
+
+	modifyArray(s)
+	fmt.Println("Won't Change the reference, since it is passed as an array", s)
+
+	ascii := map[string]int{}
+	ascii["A"] = 65
+	modifyMap(ascii)
+	fmt.Println("Will change the reference, since it is passed as a map", ascii)
+
+}
+
+func modify(slice []int){
+	for i := range slice{
+		slice[i] -= 5
+	}	
+}
+
+func modifyMap(m map[string]int){
+	m["A"] = 100
+}
+
+func modifyArray(arr [3]int){
+	for i := range arr{
+		arr[i] -=6
+	}
+}
